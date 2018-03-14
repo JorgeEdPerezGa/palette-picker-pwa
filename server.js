85ababe39942b68426e978d891df1119fe924c93
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-03-14 08:31:30 -0600

    add projets endpoint

--- server.js
+++ server.js
@@ -3,11 +3,33 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'palette picker';
+app.locals.projects = [
+  { project_id: 1,
+    name: 'project palette',
+    color_0: '0',
+    color_1: '1',
+    color_2: '2',
+    color_3: '3',
+    color_4: '4'
+  },
+  { project_id: 1,
+    name: 'project palette 2',
+    color_0: '0',
+    color_1: '1',
+    color_2: '2',
+    color_3: '3',
+    color_4: '4'
+  }
+]
 
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
   response.send('palette picker');
+})
+
+app.get('/api/v1/projects/:project_id', (request,response) => {
+  response.send(app.locals.projects);
 })
 
 app.listen(app.get('port'), () => {
