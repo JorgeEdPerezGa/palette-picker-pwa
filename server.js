3ebe00e125e65022a9283d2bed7abebe0a332f5f
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-03-20 10:37:41 -0600

    attempt redirect

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const app = express();
 // ability to parse the body of an HTTP request
 const bodyParser = require('body-parser');
 
-app.get("*", function (req, res, next) {
-  res.redirect("https://" + req.headers.host + "/" + req.path);
-});
-
 // check if there is an existing enviroment and if not redirects to localhost 3000
 app.set('port', process.env.PORT || 3000);
 
+app.get("*", (request, response, next) => {
+    response.redirect("https://" + request.headers.host + "/" + request.url);
+});
 // let app know to use bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
